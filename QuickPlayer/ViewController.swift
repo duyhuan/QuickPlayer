1fb6f76ff00b9d0d963273751b80ad2c02a24c26
techmaster <techmaster@techmasters-iMac-3.local>
2017-01-19 14:58:45 +0700

    QuickPlayer ver 1

--- QuickPlayer/ViewController.swift
+++ QuickPlayer/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var buttonPlay: UIButton!
+    @IBOutlet weak var sliderVolume: UISlider!
+    
+    var audioPlayer = AVAudioPlayer()
+    var isPlay: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        audioPlayer = try! AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: Bundle.main.path(forResource: "music", ofType: ".mp3")!) as URL)
+        audioPlayer.prepareToPlay()
+        addThumbImgForSlider()
+        
+    }
+    
+    func addThumbImgForSlider() {
+        sliderVolume.setThumbImage(UIImage(named: "thumb.png"), for: UIControlState.normal)
+        sliderVolume.setThumbImage(UIImage(named: "thumbHightLight.png"), for: UIControlState.highlighted)
+    }
+    
+    @IBAction func buttonPlayAction(_ sender: UIButton) {
+        if isPlay {
+            buttonPlay.setBackgroundImage(UIImage(named: "play.png"), for: UIControlState.normal)
+            isPlay = false
+            audioPlayer.pause()
+        } else {
+            buttonPlay.setBackgroundImage(UIImage(named: "pause.png"), for: UIControlState.normal)
+            isPlay = true
+            audioPlayer.play()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderChangeVolumeAction(_ sender: UISlider) {
+        audioPlayer.volume = sender.value
     }
-
-
 }
 
